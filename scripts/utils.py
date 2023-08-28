dbc2e6ac0db77d8ea930cea5f8a4855efab0956f
Leo Tricaud <leotricaud@gmail.com>
2023-08-28 14:18:26 +0200

    Test update

--- scripts/utils.py
+++ scripts/utils.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 class DataProcess():
 
-    def load_data():
-        path = os.path.join(os.path.dirname(os.getcwd()), "raw_data", "data.csv")
-        df = pd.read_csv(path, sep="\t")
+    def __init__(self):
+        self.path = os.path.join(os.path.dirname(os.getcwd()), "raw_data", "data.csv")
+
+    def load_data(self):
+        df = pd.read_csv(self.path, sep="\t")
         return df
