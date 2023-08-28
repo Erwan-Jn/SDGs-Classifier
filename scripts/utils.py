307d42d5912de0406acf0e0d03aedbcfd7236b0a
Leo Tricaud <leotricaud@gmail.com>
2023-08-28 18:15:18 +0200

    Added a dictionary in the class

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import string
+import numpy as np
 from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
@@ -12,13 +13,24 @@ class DataProcess():
             ---file being run"""
 
     def __init__(self):
+        '''Class carries 2 information:
+        --The path
+        --A sdg dict linking numbers to the SDG's themes'''
         self.path = os.path.join(os.path.dirname(os.getcwd()), "raw_data", "data.csv")
+        self.sdg = dict(zip(
+            [str(num) for num in np.arange(1, 17)],
+            ["Poverty", "Hunger", "Health", "Education", "Gender",
+             "Water", "Clean", "Work", "Inno", "Inequalities", "Cities",
+             "Cons&Prod", "Climate", "OceanLife", "LandLife", "Peace"]
+        )
+                        )
 
     def load_data(self):
         df = pd.read_csv(self.path, sep="\t")
         df["sdg"] = df["sdg"].astype(str)
         df["lenght_text"] = df["text"].map(lambda row: len(row.split()))
         df["nb_reviewers"] = df["labels_negative"] + df["labels_positive"]
+        df["sdg_txt"] = df["sdg"].map(self.sdg)
         return df
 
     def cleaning(sentence):
