f5fe08bd50263bf787703a2140878ef5050b9647
Leo Tricaud <leotricaud@gmail.com>
2023-09-04 16:44:22 +0200

    Some reworks on the scripts

--- scripts/utils.py
+++ scripts/utils.py
@@ -6,7 +6,6 @@ import numpy as np
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-from nltk.stem import PorterStemmer
 
 from scripts.clean_data import *
 
@@ -24,12 +23,18 @@ class DataProcess():
         --The path
         --A sdg dict linking numbers to the SDG's themes'''
         self.path = os.path.join(os.path.dirname(os.getcwd()), "raw_data", "data.csv")
-        self.sdg = dict(zip(
+
+        self.sdg = dict(zip( #Dictionnary matching sdg numbers to name
             [str(num) for num in np.arange(1, 17)],
             ["Poverty", "Hunger", "Health", "Education", "Gender",
              "Water", "Clean", "Work", "Inno", "Inequalities", "Cities",
              "Cons&Prod", "Climate", "OceanLife", "LandLife", "Peace"]
         )
+                        )
+
+        self.esg = dict(zip(np.arange(1, 17), #Dictionnary matching sdgs (16) to esgs (3)
+                            [1, 3, 3, 3, 3, 3, 2, 1, 1, 1, 2, 2, 2, 2, 2, 3]
+                            )
                         )
 
     def load_data(self):
@@ -48,7 +53,7 @@ class DataProcess():
         df["sdg_txt"] = df["sdg"].map(self.sdg)
         return df
 
-    def clean_data_short(self, agreement=0):
+    def clean_data_short(self, agreement=0, grouped=False):
         """
         Takes no argument (path given in __init__)
         Returns a pd.DataFrame
@@ -64,9 +69,14 @@ class DataProcess():
 
         df["cleaned_text"] = df["cleaned_text"].map(lambda row: " ".join([w for w in iter(word_tokenize(row)) if w not in stop_words]))
 
+        if grouped:
+            df["sdg"] = df["sdg"].astype(int)
+            df["esg"] = df["sdg"].map(self.esg)
+            return df
+
         return df
 
-    def clean_data(self, agreement=0):
+    def clean_data(self, agreement=0, grouped=False):
         """
         Takes 1 parameter, agreement. Agreement will keep all values
         above the agreement threshold for the data. The path is given
@@ -74,28 +84,21 @@ class DataProcess():
         Returns a pd.DataFrame
         Adds 3 columns of cleaned text + 1 column with length of cleaned text
         """
-
         translator_p = str.maketrans('', '', string.punctuation)
         translator_d = str.maketrans('', '', string.digits)
         stop_words = set(stopwords.words('english'))
-        word_lem = WordNetLemmatizer()
-        word_stem = PorterStemmer()
+        lemmer = WordNetLemmatizer()
 
         df = self.load_data()
         df = df.loc[df["agreement"]>=agreement, : ]
 
-        cleaned_text = clean_vec(df["text"])
-        df["lemma"] = clean_lemma_vec(cleaned_text)
-        df["stem"] = clean_stem_vec(cleaned_text)
+        df["cleaned_text"] = clean_vec(df["text"])
+        df["lemma"] = clean_lemma_vec(df["cleaned_text"])
         df["lenght_text_cleaned"] = df["lemma"].map(lambda row: len(row.split()))
 
-        return df
+        if grouped:
+            df["sdg"] = df["sdg"].astype(int)
+            df["esg"] = df["sdg"].map(self.esg)
+            return df
 
-    def group_sdg(self):
-        df = self.clean_data_short()
-        df["sdg"] = df["sdg"].astype(int)
-        sdg = np.arange(1, 17)
-        esg = [1, 3, 3, 3, 3, 3, 2, 1, 1, 1, 2, 2, 2, 2, 2, 3]
-        sdg_esg = dict(zip(sdg, esg))
-        df["esg"] = df["sdg"].map(sdg_esg)
         return df
