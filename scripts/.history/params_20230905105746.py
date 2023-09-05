9188f0c0a5233cfac482f5fe7f06794790169e5d
Leo Tricaud <leotricaud@gmail.com>
2023-09-05 12:51:44 +0200

    Reworked the paths in main.py and tweaked clean_data.py

--- scripts/.history/params_20230905105746.py
+++
@@ -1,6 +0,0 @@
-import os
-import numpy as np
-
-GCP_PROJECT = os.environ.get("GCP_PROJECT")
-BUCKET_NAME = os.environ.get("BUCKET_NAME")
-LOCAL_REGISTRY_PATH =  os.path.join(os.path.expanduser('~'), "code", "Erwan-Jn", "10-Project", "SGDs-Classifier")
