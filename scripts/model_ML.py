d768a594e2183ce03f160fe082244e8175a87d5e
Leo Tricaud <leotricaud@gmail.com>
2023-09-06 18:23:33 +0200

    Reworked worfklow

--- scripts/model_ML.py
+++ scripts/model_ML.py
@@ -1,12 +1,13 @@
 ########################### ML TEMPLATE ##############################
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split, cross_validate
-from sklearn.metrics import accuracy_score, precision_score, f1_score, recall_score, make_scorer
+from sklearn.metrics import accuracy_score, precision_score, f1_score, recall_score, classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.feature_selection import SelectKBest, chi2
 
 from colorama import Fore, Style
-from typing import Tuple
 
 import numpy as np
 import pandas as pd
@@ -16,24 +17,37 @@ import pickle
 
 from scripts.params import LOCAL_MODEL_PATH
 
+def to_arr(x):
+        return x.toarray()
+
+#ensemble = VotingClassifier(estimators=models, voting='soft')
+
 def train_model(
         X: np.ndarray,
         y: np.ndarray,
-        test_split=0.3,
-        max_features=2000
+        test_split:float=0.3,
+        max_features:int=100000
     ):
     """
     Fit the model and return a tuple (fitted_model, history)
     print(Fore.BLUE + "\nTraining model..." + Style.RESET_ALL)
     """
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_split, random_state=42, stratify=y)
 
     pipe_model = Pipeline([
-        ('tf_idf', TfidfVectorizer(max_features=max_features, ngram_range = (1, 2))),
-        ('model', LinearSVC(dual="auto", max_iter=10000))
+        ('tf_idf', TfidfVectorizer(max_features=max_features, ngram_range = (1, 3), max_df=0.8, norm="l2")),
+        ('selector', SelectKBest(chi2, k = 2000)),
+        ('clf', LogisticRegression(penalty = 'l2', C = .9, multi_class = 'multinomial',
+                                   class_weight = 'balanced', random_state = 42,
+                                   solver = 'newton-cg', max_iter = 100))
         ])
+    #('model', LogisticRegression(C = 0.8, multi_class = 'multinomial', class_weight = 'balanced',
+    #                            random_state = 42, solver = 'newton-cg', max_iter = 1000))
+    #])
 
-    res = cross_validate(pipe_model, X_train, y_train)
+    print(Fore.BLUE + "\nLaunching CV" + Style.RESET_ALL)
+
+    res = cross_validate(pipe_model, X_train, y_train, verbose=2, cv=5)
     res = pd.DataFrame(res)
 
     print(f"✅ Model trained on {len(X_train)} rows with mean cross_validated accuracy: {round(np.mean(res.get('test_score')), 2)}")
@@ -46,12 +60,12 @@ def evaluate_model(
         model,
         X: pd.DataFrame,
         y: pd.Series,
-        test_split=0.3
+        test_split:float=0.3
     ):
     """
     Evaluate trained model performance on the dataset
     """
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_split, random_state=42, stratify=y)
 
     print(Fore.BLUE + f"\nEvaluating model on {len(X_test)} rows..." + Style.RESET_ALL)
 
@@ -65,20 +79,19 @@ def evaluate_model(
                f1_score(y_test, y_pred, average="macro"), recall_score(y_test, y_pred, average="macro")]
     metrics_name = ["res_accuracy", "res_precision", "res_f1", "res_recall"]
 
-    print(f"✅ Model evaluated, accuracy: {metrics[0]}")
+    print(f"✅ Model evaluated, accuracy: {np.round(metrics[0], 2)}, precision: {np.round(metrics[1], 2)}")
+
+    print(f"✅ Full Classification Report")
+    print(classification_report(y_test, y_pred, zero_division = 0))
+
     results = dict(zip(metrics_name, metrics))
     results = {key: [value] for key, value in results.items()}
     return pd.DataFrame(results, index=[0])
 
-def predict_model(
-        model,
-        X: str
-        ):
-
+def predict_model(model, X:str) -> np.array:
     return model.predict(X), model.predict_proba(X)
 
 def load_model(model_name:str = None):
-
     if model_name==None:
         full_file_path = os.path.join(LOCAL_MODEL_PATH, "None")
     else:
